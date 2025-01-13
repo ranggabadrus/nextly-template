faffa668c1be136530bbc25b04153f46eb2e57ed
ranggabadrus <46615811+ranggabadrus@users.noreply.github.com>
2025-01-13 22:00:30 +0700

    initial commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,21 +18,21 @@ export function Footer() {
                 className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100"
               >
                 <Image
-                  src="/img/logo.svg"
+                  src="/img/logo.png"
+                  width={128}
                   alt="N"
-                  width="32"
-                  height="32"
-                  className="w-8"
+                  height={128}
+                  className="w-32"
                 />
-                <span>Nextly</span>
+                {/* <span>Nextly</span> */}
               </Link>
             </div>
 
-            <div className="max-w-md mt-4 text-gray-500 dark:text-gray-400">
+            {/* <div className="max-w-md mt-4 text-gray-500 dark:text-gray-400">
               Nextly is a free landing page & marketing website template for
               startups and indie projects. Its built with Next.js & TailwindCSS.
               And its completely open-source.
-            </div>
+            </div> */}
 
             <div className="mt-5">
               <a
@@ -52,7 +52,7 @@ export function Footer() {
           </div>
 
           <div>
-            <div className="flex flex-wrap w-full -mt-2 -ml-3 lg:ml-0">
+            {/* <div className="flex flex-wrap w-full -mt-2 -ml-3 lg:ml-0">
               {navigation.map((item, index) => (
                 <Link
                   key={index}
@@ -62,7 +62,7 @@ export function Footer() {
                   {item}
                 </Link>
               ))}
-            </div>
+            </div> */}
           </div>
           <div>
             <div className="flex flex-wrap w-full -mt-2 -ml-3 lg:ml-0">
@@ -81,50 +81,31 @@ export function Footer() {
             <div>Follow us</div>
             <div className="flex mt-5 space-x-5 text-gray-400 dark:text-gray-500">
               <a
-                href="https://twitter.com/web3templates"
+                href="https://www.linkedin.com/company/learnwithandi"
                 target="_blank"
                 rel="noopener"
               >
                 <span className="sr-only">Twitter</span>
-                <Twitter />
+                <Linkedin />
               </a>
               <a
-                href="https://facebook.com/web3templates"
-                target="_blank"
-                rel="noopener"
-              >
-                <span className="sr-only">Facebook</span>
-                <Facebook />
-              </a>
-              <a
-                href="https://instagram.com/web3templates"
+                href="https://www.instagram.com/learnwithandi/"
                 target="_blank"
                 rel="noopener"
               >
                 <span className="sr-only">Instagram</span>
                 <Instagram />
               </a>
-              <a href="https://linkedin.com/" target="_blank" rel="noopener">
-                <span className="sr-only">Linkedin</span>
-                <Linkedin />
-              </a>
             </div>
           </div>
         </div>
 
         <div className="my-10 text-sm text-center text-gray-600 dark:text-gray-400">
-          Copyright © {new Date().getFullYear()}. Made with ♥ by{" "}
-          <a href="https://web3templates.com/" target="_blank" rel="noopener">
-            Web3Templates.
-          </a>{" "}
-          Illustrations from{" "}
-          <a href="https://www.glazestock.com/" target="_blank" rel="noopener ">
-            Glazestock
-          </a>
+          Copyright © {new Date().getFullYear()}. Made with ♥ by LWA
         </div>
       </Container>
       {/* Do not remove this */}
-      <Backlink />
+      {/* <Backlink /> */}
     </div>
   );
 }
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,16 +1,37 @@
 import Image from "next/image";
 import React from "react";
 import { Container } from "@/components/Container";
-
-import userOneImg from "../../public/img/user1.jpg";
-import userTwoImg from "../../public/img/user2.jpg";
-import userThreeImg from "../../public/img/user3.jpg";
+import Landing from "../../assets/portfolio/img/Landing.png";
+import Explore from "../../assets/portfolio/img/Explore.png";
+import Explore2 from "../../assets/portfolio/img/Explore2.png";
+import Explore3 from "../../assets/portfolio/img/Explore3.png";
+import Buy from "../../assets/portfolio/img/Buy.png";
+import HistoryBuy from "../../assets/portfolio/img/HistoryBuy.png";
+import Web from "../../assets/portfolio/img/Web.png";
+import WebConfirm from "../../assets/portfolio/img/WebConfirm.png";
+import WebConfirmCash from "../../assets/portfolio/img/WebConfirmCash.png";
+import WebConfirmGetStarted from "../../assets/portfolio/img/WebConfirmGetStarted.png";
+import WebForm from "../../assets/portfolio/img/WebForm.png";
+import WebFormFilled from "../../assets/portfolio/img/WebFormFilled.png";
+import WebGetStarted from "../../assets/portfolio/img/WebGetStarted.png";
+import WebLogin from "../../assets/portfolio/img/WebLogin.png";
+import WebProfile from "../../assets/portfolio/img/WebProfile.png";
+import KickinFieldDetails from "../../assets/portfolio/img/KickinFieldDetails.png";
+import KickinHome from "../../assets/portfolio/img/KickinHome.png";
+import KickinLanding from "../../assets/portfolio/img/KickinLanding.png";
+import KickinLogin from "../../assets/portfolio/img/KickinLogin.png";
+import KickinProfile from "../../assets/portfolio/img/KickinProfile.png";
+import KickinWebFieldDetails from "../../assets/portfolio/img/KickinWebFieldDetails.png";
+import KickinWebLanding from "../../assets/portfolio/img/KickinWebLanding.png";
+import KickinWebLogin from "../../assets/portfolio/img/KickinWebLogin.png";
+import KickinWebProfile from "../../assets/portfolio/img/KickinWebProfile.png";
+import KickinWebLandingAfterLogin from "../../assets/portfolio/img/KickinWebLandingAfterLogin.png";
 
 export const Testimonials = () => {
   return (
     <Container>
       <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3">
-        <div className="lg:col-span-2 xl:col-auto">
+        {/* <div className="lg:col-span-2 xl:col-auto">
           <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
             <p className="text-2xl leading-normal ">
               Share a real <Mark>testimonial</Mark>
@@ -51,7 +72,133 @@ export const Testimonials = () => {
               title="Co-founder of Acme Inc"
             />
           </div>
-        </div>
+        </div> */}
+
+        <Image
+          alt="Landing"
+          src={Landing}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="Explore"
+          src={Explore}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="Explore2"
+          src={Explore2}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="Explore3"
+          src={Explore3}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="Buy"
+          src={Buy}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="HistoryBuy"
+          src={HistoryBuy}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="Web"
+          src={Web}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="WebConfirm"
+          src={WebConfirm}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="WebConfirmCash"
+          src={WebConfirmCash}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="WebConfirmGetStarted"
+          src={WebConfirmGetStarted}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="WebForm"
+          src={WebForm}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="WebFormFilled"
+          src={WebFormFilled}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="WebGetStarted"
+          src={WebGetStarted}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="WebLogin"
+          src={WebLogin}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="WebProfile"
+          src={WebProfile}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="KickinFieldDetails"
+          src={KickinFieldDetails}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="KickinHome"
+          src={KickinHome}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="KickinLanding"
+          src={KickinLanding}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="KickinLogin"
+          src={KickinLogin}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="KickinProfile"
+          src={KickinProfile}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="KickinWebFieldDetails"
+          src={KickinWebFieldDetails}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="KickinWebLanding"
+          src={KickinWebLanding}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="KickinWebLandingAfterLogin"
+          src={KickinWebLandingAfterLogin}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="KickinWebLogin"
+          src={KickinWebLogin}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
+        <Image
+          alt="KickinWebProfile"
+          src={KickinWebProfile}
+          style={{ width: "300px", height: "auto", margin: "10px" }}
+        />
       </div>
     </Container>
   );
